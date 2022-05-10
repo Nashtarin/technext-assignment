86f3eb3947cd3b53beef912779df8ffff38322b8
Nashtarin Nur <nashtarin.nur@gmail.com>
2022-05-10 13:22:59 +0600

    card complete

--- src/pages/Space/SingleCard/SingleCard.js
+++ src/pages/Space/SingleCard/SingleCard.js
@@ -6,12 +6,13 @@ const SingleCard = ({spaceData}) => {
         <div className="col">
 
         <div className="card border border-primary rounded-3 shadow  product h-100">
-            {/* <img src={img} className="card-img-top" alt="..." /> */}
+            <img src={spaceData.links.mission_patch_small} className="card-img-top img-thumbnail" alt="..." />
             <div className="card-body">
 
-                <h4  className="card-title text-primary fw-bold">{spaceData.mission_name}</h4>
-                {/* <p className="card-text">{description}</p> */}
-                {/* <h4 style={{ color: 'hotpink' }} className="fw-bold">Price:${price}</h4> */}
+                <h5  className="card-title text-primary fw-bold">Mission: <i>{spaceData.mission_name}</i></h5>
+                <p className="card-text">{spaceData.details}</p>
+                <h6>Launch Date:<i>{spaceData.launch_date_utc}</i></h6>
+                <h6>Rocket:<i>{spaceData.rocket.rocket_name},type:{spaceData.rocket.rocket_type}</i></h6>
                 {/* <Link to={`/products/${_id}`}>
                     <Button style={{ backgroundColor: "hotpink", border: "2px solid purple" }} className="text-white fw-bold ">Purchase</Button>
                 </Link> */}
